ef43feba1839dae84a16499f52a23ff8a258b034
Karthick <karthicklingasamy6@gmail.com>
2026-08-20 12:29:55 +0530

    fix: move Flutter build to script to fix 256-char limit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+flutter/bin/flutter config --no-analytics
+flutter/bin/flutter pub get
+flutter/bin/flutter build web --release
